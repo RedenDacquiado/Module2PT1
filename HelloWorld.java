36277d202ceb2af4fabcac1519dce1baaf5bcd10
RedenDacquiado <Rijdacquiado@mcm.edu.ph>
2025-01-28 12:46:12 +0800

    Module2PT1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,6 @@
-import java.util.Scanner;
 public class HelloWorld{
     public void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = input.nextLine();
-
-        System.out.println("What is your favorite programming language? ");
-        String language = input.nextLine();
-
-        System.out.println("Hello, " + name + "! It's great that you love " + language + ".");
-
+        System.out.println("Hello, my name is Reden Isaac Joshua E. Dacquiado");
+        System.out.println("I ma excited to learn Java!");
     }
 }
--- PT1.java
+++
@@ -1,5 +0,0 @@
-public class PT1{
-    public void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
